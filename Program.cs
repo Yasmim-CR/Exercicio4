502d8e53859f9d3251f59d0c726ff8fbf448da08
Yasmim de Castro <Yas.castr0.r@gmail.com>
2025-03-17 10:10:56 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+
+Console.ForegroundColor= ConsoleColor.White;
+Console.Write("C");
+
+Console.ForegroundColor= ConsoleColor.Red;
+Console.Write("O");
+
+Console.ForegroundColor= ConsoleColor.Black;
+Console.Write("R");
+
+Console.ForegroundColor= ConsoleColor.White;
+Console.Write("I");
+
+Console.ForegroundColor= ConsoleColor.Red;
+Console.Write("N");
+
+Console.ForegroundColor= ConsoleColor.Black;
+Console.Write("T");
+
+Console.ForegroundColor= ConsoleColor.White;
+Console.Write("H");
+
+Console.ForegroundColor= ConsoleColor.Red;
+Console.Write("I");
+
+Console.ForegroundColor= ConsoleColor.Black;
+Console.Write("A");
+
+Console.ForegroundColor= ConsoleColor.White;
+Console.Write("N");
+
+Console.ForegroundColor= ConsoleColor.Red;
+Console.Write("S");
+
+Console.ResetColor();
+Console.ReadKey();
